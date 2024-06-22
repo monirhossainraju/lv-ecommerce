d355b7bcf6ad549eeab7569416b43f2d72b3de19
monirhossainraju <monirh111@gmail.com>
2024-06-22 17:33:55 +0600

    multi dashboard configured

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,3 +19,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('admin/dashboard', [HomeController::class,'index']);
